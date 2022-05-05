08f1025dc65fe21d84190dcacb85d34e3b71c6f8
Abhisek <aviseqkhadgi1@gmail.com>
2022-05-05 11:27:38 +0545

    Procfile moved

--- main/main/settings.py
+++ main/main/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,4 +145,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 LOGIN_REDIRECT_URL = '/'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
