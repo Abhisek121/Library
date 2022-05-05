068d9aeacfd4e477b69e198a63c972945bd71177
Abhisek <aviseqkhadgi1@gmail.com>
2022-05-05 11:03:28 +0545

    import django_heroku

--- main/main/settings.py
+++ main/main/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from threading import local
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,3 +145,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 LOGIN_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
